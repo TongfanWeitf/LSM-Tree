7773e161fd328db7f82c4092336cbbd4acf10777
TongfanWeitf <113316476+TongfanWeitf@users.noreply.github.com>
2024-04-20 04:35:32 +0800

    Update lsm_tree.cc

--- lsm_tree.cc
+++ lsm_tree.cc
@@ -65,10 +65,11 @@ int main(int argc, char *argvx[]) {
     }
 
     // Write the meta file
+    std::cout << "writing data" <<std::endl ;
     std::string metadataPath = "./data/metadata.meta";
     writeMetadataToDisk(*DiskMetaFile::getInstance(), metadataPath);
   }
-
+ 
   printEmulationOutput(_env);
 
   return 0;
